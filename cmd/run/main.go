80165f203a3a3337c844040efa5490af0b83f7b0
suwer <admin@suwer.ru>
2017-09-25 10:13:15 +0300

    Add main

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"github.com/dmitryrpm/go-finder/finder"
+	"github.com/dmitryrpm/go-finder/config"
+	"log"
+	"os"
+	"bufio"
+	"io"
+)
+
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	run(os.Stdin, log.New(os.Stderr, "", 0), cfg)
+}
+
+func run(reader io.Reader, stdout *log.Logger, cfg *config.Config){
+	p := finder.NewPool(cfg.K, stdout)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		if cfg.Type == "url" {
+			p.Put(&finder.WebTask{
+				Source: scanner.Text(),
+			})
+		} else {
+			p.Put(&finder.FileTask{
+				Source: scanner.Text(),
+			})
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	p.StopPoolAndWait()
+
+	stdout.Printf("Total: %d", p.Total)
+}
---
+++ cmd/run/main_test.go
@@ -0,0 +1,67 @@
+// build integration
+
+package main
+
+import (
+	"github.com/dmitryrpm/go-finder/config"
+	"bytes"
+	"fmt"
+	"github.com/pborman/uuid"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+type mockLog struct {
+	buf bytes.Buffer
+}
+
+func (m *mockLog) Write(p []byte) (n int, err error) {
+	m.buf.Write(p)
+	return 0, nil
+}
+
+func TestSuccessUrl(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Go Go Go")
+	}))
+	defer server.Close()
+	buf := bytes.NewBuffer([]byte(fmt.Sprintf("%s\n%s\n%s\n", server.URL, server.URL, server.URL)))
+	mockL := mockLog{buf: bytes.Buffer{}}
+	stdout := log.New(&mockL, "", 0)
+	cfg, err := config.NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	run(buf, stdout, cfg)
+	res := fmt.Sprintf("Count for %s: 3\nCount for %s: 3\nCount for %s: 3\nTotal: 9\n", server.URL, server.URL, server.URL)
+	if mockL.buf.String() != res {
+		t.Fatalf("\n'%s'\n!=\n'%s'", res, mockL.buf.String())
+	}
+}
+
+func TestSuccessFile(t *testing.T) {
+	filename := "/tmp/file_test" + uuid.New()
+
+	b := []byte("hello\nGo\n")
+	err := ioutil.WriteFile(filename, b, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(filename)
+
+	buf := bytes.NewBuffer([]byte(fmt.Sprintf("%s\n%s\n%s\n", filename, filename, filename)))
+	mockL := mockLog{buf: bytes.Buffer{}}
+	stdout := log.New(&mockL, "", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	run(buf, stdout, &config.Config{K: 5, Type: "file"})
+	res := fmt.Sprintf("Count for %s: 1\nCount for %s: 1\nCount for %s: 1\nTotal: 3\n", filename, filename, filename)
+	if mockL.buf.String() != res {
+		t.Fatalf("\n'%s'\n!=\n'%s'", res, mockL.buf.String())
+	}
+}
